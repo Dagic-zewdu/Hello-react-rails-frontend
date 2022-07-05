3f3f505d599c54b6428d8aba6f809305638990a2
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-05 13:00:56 +0300

    Linked boostrap file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './Redux/configureStore';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
