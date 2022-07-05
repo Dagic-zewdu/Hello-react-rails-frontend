ab7c53732f93be6e11ebed54bd374ef5d9dbaa28
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-05 12:44:00 +0300

    Modified router

--- src/route.js
+++ src/route.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Details from './components/Details/Details';
 import Home from './components/Home/Home';
+import Message from './components/message';
 
 function Router() {
   return (
     <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/details/:id" element={<Details />} />
+      <Route exact path="/" element={<Message />} />
+      <Route exact path="/message" element={<Message />} />
     </Routes>
   );
 }
