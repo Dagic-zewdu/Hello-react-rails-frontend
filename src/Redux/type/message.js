fb50c7eb5ac9e1f728a331573453e98f60ba1507
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-05 11:14:24 +0300

    Added Message action types

---
+++ src/Redux/type/message.js
@@ -0,0 +1,7 @@
+const MessageActionTypes = {
+  FETCH_MESSAGE_START: 'hello-react-rails/messages/FETCH_MESSAGES_START',
+  FETCH_MESSAGE_SUCCESS: 'hello-react-rails/messages/FETCH_MESSAGES_SUCCESS',
+  FETCH_MESSAGE_ERROR: 'hello-react-rails/messages/FETCH_MESSAGES_ERROR',
+};
+
+export default MessageActionTypes;
