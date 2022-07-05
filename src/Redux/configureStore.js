587cb62d7fff19c0437cc2c320d89cbf16435ce8
Dagi <dagi.zewdu.dz@gmail.com>
2022-07-05 19:55:44 +0300

    updated main branch

--- src/Redux/configureStore.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import MessagesReducer from './reducer/message';
-
-const store = createStore(MessagesReducer, applyMiddleware(thunk));
-export default store;
